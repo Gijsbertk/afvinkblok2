5adfd9b887ddddd005cfa09457aabc993b27fe01
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2020-01-22 17:53:07 +0100

    Add files via upload

---
+++ afv7.py
@@ -0,0 +1,22 @@
+import tkinter
+from tkinter import messagebox
+
+def main():
+
+    def lezen():
+
+        """ leest bestand in en haalt indien nodig header weg (omdat tsv) je filter alle goede genen weg met een
+        regular expression"""
+
+    def lijst():
+        """maak 2d lijst waarbij alle onnodige troep eruit is gehaald waarna je met de lijst een grfaiek kan maken"""
+
+    def alias():
+        """met de 2d lijst die er is gemaakt kan je kijken met een 2d lijst MET header om de info van het gen
+        te linken aan de header en dit in een dic op te slaan"""
+
+    def gui():
+        """je maakt een gui waarmee een scherm op popt en iemand de gen accessiecode kan invullen waarna er een
+        messagebox op popt met de info. de info van het gen wordt beschikaar gemaakt door dit te koppelen aan een dic en
+        de value van de key te tonen"""
+
