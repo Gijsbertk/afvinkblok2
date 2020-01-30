8a6dbf4b9e8650fc1dea40815628161ed6a6f7bf
Gijsbertk <55399498+Gijsbertk@users.noreply.github.com>
2020-01-30 22:26:48 +0100

    Update afvink2_bar.py

--- afvink2.1/afvink2_bar.py
+++ afvink2.1/afvink2_bar.py
@@ -3,18 +3,24 @@ import matplotlib.pyplot as plt
 #Datum: November 2019
 def main():
     een()
-    """de main roept de funtie aan"""
+    """
+    de main roept de funtie aan
+    """
 
 
 def een():
-    """Deze functie geeft 2 variabelen een waarde waarna deze in een lijst
-gepusht worden en gebruikt worden om de staafdiagram te maken en te weergeven"""
+    """
+    Deze functie geeft 2 variabelen een waarde waarna deze in een lijst
+gepusht worden en gebruikt worden om de staafdiagram te maken en te weergeven
+:param: geen
+:return: geen
+"""
     janssen = 5
     Berends = 5
 
 
-    hoogte = [5, 5]
-    naam = ['Berends', 'Janssen']
+    hoogte = [5, 5]  # maakt de hoogte voor de staven
+    naam = ['Berends', 'Janssen']  # geeft de staven een naam
 
     plt.bar(range(len(hoogte)), hoogte, tick_label= naam )
     plt.show()
